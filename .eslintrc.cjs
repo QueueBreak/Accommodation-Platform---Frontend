a417914b22894cefa55f3827c226a1cf1b65573d
Adrian Taoana <adrianmihai441@yahoo.com>
2023-12-31 02:25:08 +0200

    Added eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,38 @@ module.exports = {
 	env: {browser: true, es2020: true},
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:import/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
 		'plugin:react-hooks/recommended',
+		'plugin:react/jsx-runtime',
+		'prettier',
+		'eslint-config-prettier'
 	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+				extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
+			},
+		},
+	},
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json'],
+		tsconfigRootDir: __dirname,
+	},
 	plugins: ['react-refresh'],
 	rules: {
-		'react-refresh/only-export-components': [
-			'warn',
-			{allowConstantExport: true},
-		],
+		'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+		'no-console': 'warn'
 	},
 }
